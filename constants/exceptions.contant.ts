1e7d017ab7d48b1a5cb8a265b3e3293eb38e6191
Orel Balilti <orelba93@gmail.com>
2018-08-21 21:22:14 +0300

    missing execption added

--- constants/exceptions.contant.ts
+++ constants/exceptions.contant.ts
@@ -1,6 +1,7 @@
 import {IExceptionConstant} from "@sugoi/core";
 
 export const EXCEPTIONS: { [prop: string]: IExceptionConstant } = {
+    INVALID: {code: 4000, message: "INVALID"},
     NOT_IMPLEMENTED: {message:"Implementation is missing",code:5000},
     CONFIGURATION_MISSING : {message:"Connection configuration is missing",code:5001}
 }
